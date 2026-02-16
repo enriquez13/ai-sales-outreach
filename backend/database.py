06bbd9cbae9eca37eb0023bc5c3dd3179d6d2c3b
Alejandro <alejo.enriquez@hotmail.com>
2026-02-16 01:31:00 -0300

    json3

--- backend/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-engine = create_engine("sqlite:///leads.db", connect_args={"check_same_thread": False})
-
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
